ca7d9f6116be464ec0c1c4a795311a5aadc8973f
codingbat <Nazmul.Alam@students.ittralee.ie>
2015-12-15 12:59:56 +0000

    Tested searchForSong in the Main class

--- src/ie/ittralee/Main.java
+++ src/ie/ittralee/Main.java
@@ -7,25 +7,45 @@ public class Main {
 
     public static void main(String[] args) {
         SongLibrary songLibrary = new SongLibrary();
+        Member newMember = new Member(1, "Nazmul Alam");
 
         Set<Genre> genres = new HashSet<Genre>();
         genres.add(Genre.JAZZ);
         genres.add(Genre.BLUES);
         genres.add(Genre.RNB);
-        songLibrary.addSong("I'm so blue", genres);
-
+        songLibrary.addSong("I'm so blue", genres, newMember);
+        songLibrary.addSong("I'm so blue", genres, new Member(2, "Ciaran Griffo"));
 
         genres = new HashSet<Genre>();
         genres.add(Genre.POP);
         genres.add(Genre.ROCK);
-        songLibrary.addSong("Slapping Gourgous", genres);
+        songLibrary.addSong("Slapping Gourgous", genres, newMember);
 
-        Song searchedSong = songLibrary.searchForSong("I'm so blue");
+        Song searchedSong = songLibrary.searchForSongByTitle("I'm so blue");
         if (searchedSong == null)
             System.out.println("Song not found.");
         else
             System.out.println(searchedSong.toString());
 
+        searchedSong = songLibrary.searchForSongByArtistName("Nazmul Alam");
+        if (searchedSong == null)
+            System.out.println("Song not found.");
+        else
+            System.out.println(searchedSong.toString());
+
+
+        searchedSong = songLibrary.searchForSong("I'm so blue", "Nazmul Alam");
+        if (searchedSong == null)
+            System.out.println("Song not found.");
+        else
+            System.out.println(searchedSong.toString());
+
+
+        searchedSong = songLibrary.searchForSong("I'm so blue", "Ciaran Griffo");
+        if (searchedSong == null)
+            System.out.println("Song not found.");
+        else
+            System.out.println(searchedSong.toString());
 
     }
 }
--- src/ie/ittralee/Song.java
+++ src/ie/ittralee/Song.java
@@ -38,7 +38,6 @@ public class Song{
     }
 
 
-
     /**
      * @return the songId
      */
